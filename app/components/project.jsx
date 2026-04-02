0bb1b55929af702b8822279870d0a23d864baaf6
eslam alelwany <118006782+Eslamalelwany@users.noreply.github.com>
2026-04-02 15:04:36 +0200

    Add new updates

--- app/components/project.jsx
+++ app/components/project.jsx
@@ -5,80 +5,80 @@ export default function Projects() {
         <div className="projects-container">
 {/* ONE */}
             <div className="panel panel1">
-                <div className="baby w-[95vw] md:h-[72vh] h-[65vh] lg:h-[90vh] xl:h-[89.5vh] bg-[#444444]  border-[#301CA0] lg:p-10 p-5 rounded-xl lg:w-[80vw] xl:w-[80vw] mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
+                <div className="baby w-[95vw] md:h-[72vh] h-[65vh] lg:h-[90vh] xl:h-[89.5vh] bg-[#003049]  border-[#301CA0] lg:p-10 p-5 rounded-xl lg:w-[80vw] xl:w-[80vw] mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
                     <h3 className="w-full text-lg lg:text-xl">E-commerce store</h3>
                     <div className="pt-4 pb-4 w-full md:w-[70%] mr-auto">
                         <img className="w-full h-full rounded-xl" src="/images/photo_2026-01-05_12-15-20.jpg" alt="" />
                     </div>
                     <p className="w-full text-xs md:text-xl">React.JS, JavaScript, Tailwind, Redux</p>
-                    <a rel="noopener noreferrer" target="_blank" className="bg-[#301CA0] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://westedition.netlify.app/">View Livedemo</a>
+                    <a rel="noopener noreferrer" target="_blank" className="bg-[#132440] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://westedition.netlify.app/">View Livedemo</a>
                 </div>
             </div>
 
 
 {/* TWO */}
             <div className="panel panel2">
-                <div className=" baby w-[95vw] bg-[#686868] h-[65vh] md:h-[72vh] lg:h-[90vh] xl:h-[89.5vh]   border-[#301CA0] lg:p-10 p-5 rounded-xl lg:w-[80vw] xl:w-[80vw] mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
+                <div className=" baby w-[95vw] bg-[#234C6A] h-[65vh] md:h-[72vh] lg:h-[90vh] xl:h-[89.5vh]   border-[#301CA0] lg:p-10 p-5 rounded-xl lg:w-[80vw] xl:w-[80vw] mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
                     <h3 className="w-full text-lg lg:text-xl">Recipe finder</h3>
 
                     <div className=" pt-4 pb-4 w-full md:w-[70%] mr-auto">
                         <img className="w-full h-full rounded-xl" src="/images/poster.jpeg" alt="" />
                     </div>
                     <p className="w-full text-xs md:text-xl">React.JS, JavaScript, Tailwind, Redux</p>
-                    <a rel="noopener noreferrer" target="_blank" className="bg-[#301CA0] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://recipe-finder30.netlify.app/">View Livedemo</a>
+                    <a rel="noopener noreferrer" target="_blank" className="bg-[#132440] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://recipe-finder30.netlify.app/">View Livedemo</a>
                 </div>
             </div>
 
 {/* THREE */}
             <div className="panel panel3">
-                <div className=" baby bg-[#444444] md:h-[72vh] h-[65vh] w-[95vw] lg:h-[90vh] xl:h-[89.5vh] lg:w-[80vw] xl:w-[80vw]   border-[#301CA0] lg:p-10 p-5 rounded-xl  mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
+                <div className=" baby bg-[#003049] md:h-[72vh] h-[65vh] w-[95vw] lg:h-[90vh] xl:h-[89.5vh] lg:w-[80vw] xl:w-[80vw]   border-[#301CA0] lg:p-10 p-5 rounded-xl  mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
                     <h3 className="w-full text-lg lg:text-xl">weather-app</h3>
 
                     <div className="pt-4 pb-4 w-full md:w-[70%] mr-auto">
                         <img className="w-full h-full rounded-xl" src="/images/photo_2026-01-04_15-53-18.jpg" alt="" />
                     </div>
                     <p className="w-full text-xs md:text-xl">React.JS, JavaScript, Tailwind, Redux</p>
-                    <a rel="noopener noreferrer" target="_blank" className="bg-[#301CA0] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://weatherreduxx.netlify.app/">View Livedemo</a>
+                    <a rel="noopener noreferrer" target="_blank" className="bg-[#132440] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://weatherreduxx.netlify.app/">View Livedemo</a>
                 </div>
             </div>
 
 {/* FOUR */}
             <div className="panel panel4">
-                <div className=" baby bg-[#686868] md:h-[72vh] h-[65vh]  w-[95vw] lg:h-[90vh] xl:h-[89.5vh]  border-[#301CA0] lg:p-10 p-5 rounded-xl lg:w-[80vw] xl:w-[80vw] mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
+                <div className=" baby bg-[#234C6A] md:h-[72vh] h-[65vh]  w-[95vw] lg:h-[90vh] xl:h-[89.5vh]  border-[#301CA0] lg:p-10 p-5 rounded-xl lg:w-[80vw] xl:w-[80vw] mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
                     <h3 className="w-full text-lg lg:text-xl">Landing page</h3>
 
                     <div className="pt-4 pb-4 w-full md:w-[70%] mr-auto">
                         <img className="w-full h-full rounded-xl" src="/images/photo_2026-01-04_17-27-06.jpg" alt="" />
                     </div>
                     <p className="w-full text-xs md:text-xl">HTML, CSS, JavaScript</p>
-                    <a rel="noopener noreferrer" target="_blank" className="bg-[#301CA0] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://eslamalelwany.github.io/software-house-site/">View Livedemo</a>
+                    <a rel="noopener noreferrer" target="_blank" className="bg-[#132440] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://eslamalelwany.github.io/software-house-site/">View Livedemo</a>
                 </div>
             </div>
 
 {/* FIVE */}
             <div className="panel panel4">
-                <div className= " baby  bg-[#444444] md:h-[72vh] h-[65vh] lg:h-[90vh] xl:h-[89.5vh] w-[95vw] lg:w-[80vw] xl:w-[80vw]   border-[#301CA0] lg:p-10 p-5 rounded-xl  mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
+                <div className= " baby  bg-[#003049] md:h-[72vh] h-[65vh] lg:h-[90vh] xl:h-[89.5vh] w-[95vw] lg:w-[80vw] xl:w-[80vw]   border-[#301CA0] lg:p-10 p-5 rounded-xl  mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
                     <h3 className="w-full text-lg lg:text-xl">Hangman game</h3>
 
                     <div className="pt-4 pb-4 w-full md:w-[70%] mr-auto">
                         <img className="w-full h-full rounded-xl" src="/images/photo_2026-01-04_17-34-54.jpg" alt="" />
                     </div>
                     <p className="w-full text-xs md:text-xl">HTML, CSS, JavaScript</p>
-                    <a rel="noopener noreferrer" target="_blank" className="bg-[#301CA0] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://eslamalelwany.github.io/hangman/">View Livedemo</a>
+                    <a rel="noopener noreferrer" target="_blank" className="bg-[#132440] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://eslamalelwany.github.io/hangman/">View Livedemo</a>
                 </div>
             </div>
 
 
 {/* Six */}
                         <div className="panel panel4">
-                <div className=" baby bg-[#686868] md:h-[72vh] h-[65vh] lg:h-[90vh] xl:h-[89.5vh]  w-[95vw]  lg:w-[80vw] xl:w-[80vw] border-[#301CA0] lg:p-10 p-5 rounded-xl mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
+                <div className=" baby bg-[#234C6A] md:h-[72vh] h-[65vh] lg:h-[90vh] xl:h-[89.5vh]  w-[95vw]  lg:w-[80vw] xl:w-[80vw] border-[#301CA0] lg:p-10 p-5 rounded-xl mx-auto text-2xl lg:text-5xl flex flex-col md:flex-row flex-wrap justify-center lg:items-center font-bold">
                     <h3 className="w-full text-lg lg:text-xl">Portfolio project</h3>
 
                     <div className="pt-4 pb-4 w-full md:w-[70%] mr-auto">
                         <img className="w-full h-full rounded-xl" src="/images/WhatsApp Image 2026-01-28 at 5.17.20 AM.jpeg" alt="" />
                     </div>
                     <p className="w-full text-xs md:text-xl">REACT.JS, Javascript</p>
-                    <a rel="noopener noreferrer" target="_blank" className="bg-[#301CA0] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://portfolio-39.netlify.app/">View Livedemo</a>
+                    <a rel="noopener noreferrer" target="_blank" className="bg-[#132440] p-3 mt-5 font-extralight mr-auto text-white rounded-xl text-sm" href="https://portfolio-39.netlify.app/">View Livedemo</a>
                 </div>
             </div>
 
